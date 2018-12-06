404498be13992d10951e699113b011bf753caa6a
Anthony Ou <ou.anthony@gmail.com>
2018-12-06 12:04:07 -0700

    more options

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ in nodejs form.
 
 Mainly for CI and CD integrations rather than include this in your codebase
 or install the aws cli everytime, if you have nodejs handy you could use this script.
+
+# Usage
+
+```
+Usage: ecr-login [options]
+
+Options:
+-e, --echo print to stdout
+-r, --region [region] aws region (default: "ca-central-1")
+-k, --key [key] aws api key
+-s, --secret [secret] aws api key secret
+-h, --help output usage information
+```
--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 const AWS = require('aws-sdk');
 const commander = require('commander');
+const { spawn } = require('child_process');
 
-commander.option('-r, --region', 'aws region', 'ca-central-1');
-commander.option('-k, --key', 'aws api key');
-commander.option('-s, --secret', 'aws api key secret');
+commander
+  .option('-e, --echo', 'print to stdout', false)
+  .option('-r, --region [region]', 'aws region', 'ca-central-1')
+  .option('-k, --key [key]', 'aws api key')
+  .option('-s, --secret [secret]', 'aws api key secret')
+  .parse(process.argv);
+
 const ecr = new AWS.ECR({
   apiVersion: '2015-09-21',
   region: commander.region,
@@ -35,12 +40,12 @@ ecr
       .split(':');
     const proxyEndpoint = authorizationData[0].proxyEndpoint;
 
-    await childProcessP.spawn('docker', [
-      'login',
-      '-u',
-      user,
-      '-p',
-      pass,
-      proxyEndpoint,
-    ]);
+    const command = ['login', '-u', user, '-p', pass, proxyEndpoint];
+    if (commander.echo) {
+      console.log('docker ' + command.join(' '));
+    } else {
+      const child = spawn('docker', command);
+      child.stdin.pipe(process.stdin);
+      child.stdout.pipe(process.stdout);
+    }
   });
