2e607eac5e40b16b633b6042546421ebe42302b7
Anthony Ou <ou.anthony@gmail.com>
2018-12-06 20:30:21 -0700

    remove axios

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const { spawn } = require('child_process');
 const awscred = require('awscred');
 const https = require('https');
 
-
 awscred.load(async (err, { credentials }) => {
   commander
     .option('-e, --echo', 'print to stdout', false)
     .option('-r, --region [region]', 'aws region', 'ca-central-1')
-    .option('-k, --key [key]', 'aws api key', credentials.accessKeyId)
-    .option('-s, --secret [secret]', 'aws api key secret', credentials.secretAccessKey)
+    .option('-k, --key [key]', 'aws api key in ENV or config')
+    .option('-s, --secret [secret]', 'aws api key secret in ENV or config')
     .parse(process.argv);
 
   if (err) throw err;
@@ -30,31 +29,33 @@ awscred.load(async (err, { credentials }) => {
   };
 
   const sign = aws4.sign(opts, {
-    accessKeyId: commander.key,
-    secretAccessKey: commander.secret,
+    accessKeyId: commander.key || credentials.accessKeyId,
+    secretAccessKey: commander.secret || credentials.secretAccessKey,
   });
 
+  const res = await new Promise(resolve => {
+    const req = https.request(
+      {
+        hostname: sign.hostname,
+        port: 443,
+        method: sign.method,
+        path: sign.path,
+        headers: sign.headers,
+      },
+      r => {
+        var body = '';
+        r.on('data', function(chunk) {
+          body = body + chunk;
+        });
 
-  const res = await new Promise((resolve) => {
-    const req = https.request({
-      hostname: sign.hostname,
-      port: 443,
-      method: sign.method,
-      path: sign.path,
-      headers: sign.headers,
-    }, r => {
-      var body = '';
-      r.on('data', function (chunk) {
-        body = body + chunk;
-      });
-
-      r.on('end', function () {
-        resolve(JSON.parse(body));
-      });
-    });
-    req.write(sign.body)
-    req.end()
-  })
+        r.on('end', function() {
+          resolve(JSON.parse(body));
+        });
+      }
+    );
+    req.write(sign.body);
+    req.end();
+  });
   const { authorizationData } = res;
   const [user, pass] = Buffer.from(
     authorizationData[0].authorizationToken,
